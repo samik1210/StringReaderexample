af2d14d3e6b87c41068ed58ff6cbe39253f3fab5
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:21:03 +0530

    Add project files.

---
+++ StringReaderexample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCharacters();
+        }
+
+        static async void ReadCharacters()
+        {
+            StringBuilder stringToRead = new StringBuilder();
+            stringToRead.AppendLine("Characters in 1st line to read");
+            stringToRead.AppendLine("samiksha");
+            stringToRead.AppendLine("parag");
+
+            using (StringReader reader = new StringReader(stringToRead.ToString()))
+            {
+                string readText = await reader.ReadToEndAsync();
+                Console.WriteLine(readText);
+            }
+        }
+    }
+}
